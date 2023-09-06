672debdc65e1cb13aedc0cc67326abb61c0ab9af
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-06 17:11:40 +0300

    ProductResponse done

--- src/main/java/com/techg/productservice/dto/ProductResponse.java
+++ src/main/java/com/techg/productservice/dto/ProductResponse.java
@@ -1,2 +1,19 @@
-package com.techg.productservice.dto;public class ProductResponse {
+package com.techg.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data  //creates getter and setters
+@Builder  //creates builder methods
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
 }
