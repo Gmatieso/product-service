b04743c55e4404044e86299f12651973f1634a48
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-06 22:59:33 +0300

    getAllProducts Service done

--- src/main/java/com/techg/productservice/service/ProductService.java
+++ src/main/java/com/techg/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.techg.productservice.service;
 
 import com.techg.productservice.dto.ProductRequest;
+import com.techg.productservice.dto.ProductResponse;
 import com.techg.productservice.model.Product;
 import com.techg.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 //@RequiredArgsConstructor
@@ -25,9 +28,25 @@ public class ProductService {
                 .name(productRequest.getName())
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
-                .build();
+                .build(); //creates an object of type product
         //save your product to the repository
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+      List<Product> products =   productRepository.findAll();
+      //maps Product class into ProductResponse class
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    //Creating object of productResponse
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
